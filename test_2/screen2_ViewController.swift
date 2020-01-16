78d312eb7cc801d2b127749f931e5062d621d178
Vo Minh Hoang <hoangvm.hiephoa@gmail.com>
2020-01-16 02:33:11 -0800

    Add pickerview and show value

--- test_2/screen2_ViewController.swift
+++ test_2/screen2_ViewController.swift
@@ -8,22 +8,46 @@
 
 import UIKit
 
-class screen2_ViewController: UIViewController {
+class screen2_ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+
+    
+    //---Outlet
     @IBOutlet weak var outputLabel: UILabel!
     @IBOutlet weak var screen2Button: UIButton!
+    @IBOutlet weak var genderPickerView: UIPickerView!
+    @IBOutlet weak var resultPickerView: UILabel!
+    //---Paramater
     var biendb_2:UserDefaults=UserDefaults()
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        outputLabel.text=biendb_2.value(forKey: "data") as! String
-        // Do any additional setup after loading the view.
-    }
+    var arrData:Array<[String]>=[["Male", "Female","Other"],["Vo Minh Hoang","Le Ngoc Bao Tran","Vo Hoang Ngoc","Nguyen Thien Bao"]]
+    //---Action
     @IBAction func showScreen1ButtonAction(_ sender: Any) {
         let scr = storyboard?.instantiateViewController(withIdentifier: "screen_1") as! screen1_ViewController
         
         present(scr, animated: false, completion: nil)
     }
+    //---------------------------
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        outputLabel.text=biendb_2.value(forKey: "data") as! String
+        genderPickerView.delegate=self
+        genderPickerView.dataSource=self
+        // Do any additional setup after loading the view.
+    }
+    //---Function
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return arrData.count
+    }
     
-
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return arrData[component].count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return arrData[component][row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        resultPickerView.text=arrData[component][row]
+    }
     /*
     // MARK: - Navigation
 
